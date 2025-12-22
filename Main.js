b3ba4f91511658c78f3890db3221d1a933bec234
Jack <leovski76@gmail.com>
2025-12-22 10:21:26 +0500

    Сделал Main.js связующим элементом между модулями

--- Main.js
+++ Main.js
@@ -1,29 +1,6 @@
 
-// Импорт функции для добавления слов в словарь
-import { addWord } from './addWord.js';
-import readline from "node:readline/promises";
-import { stdin, stdout } from "node:process";
+import { startCli } from "./UI/cli.js";
+// import { select } from "@inquirer/prompts";
+// import { addWord, showWords } from "./dictionaryFunctions.js";
 
-const rl = readline.createInterface({ stdin, stdout });
-
-async function start() {
-    console.log("\nЭто мой казахско-русский словарь.");
-    console.log("Я сделал эту программу, чтобы создавать удобные списки для заучивания\n");
-
-    const startQuestion = await rl.question("Готов начать? (да/нет)  ").toLowerCase();
-
-    if (startQuestion === "да") {
-        console.log("Поехали...\n");
-
-    } else {
-        rl.close();
-        console.log("Окей! Тогда завершаю программу.");
-        return;
-    }
-}
-
-// function howManyWords() {
-//     const 
-
-
-addWord();
+startCli();
