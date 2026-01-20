ad4ee4cd51451f913d4662a7a4fae5e040bddd5c
Jack <leovski76@gmail.com>
2026-01-20 21:19:24 +0500

    refactor(use case): showWords выводит результат по запросу пользователя

--- application/showWords.js
+++ application/showWords.js
@@ -1,8 +1,33 @@
 
 
-export async function showAllWords(repository) {
-    const dictionaries = await repository.loadAll();
-    const allWords = dictionaries.flatMap( dict => 
-        Object.entries(dict));
-    return allWords;
+export async function showWords(query, repository) {
+  switch (query.type) {
+    case "all":
+      return showAll(repository);
+
+    case "letter":
+      return showByLetter(query.letter, repository);
+
+    case "word":
+      return showSingle(query.word, repository);
+
+    default:
+      throw new Error("Unknown showWords query type");
+  }
+}
+
+async function showAll(repository) {
+  const dictionaries = await repository.loadAll();
+  return dictionaries.flatMap(dict => Object.entries(dict));
+}
+
+async function showByLetter(letter, repository) {
+  const dict = await repository.loadByLetter(letter);
+  return Object.entries(dict);
+}
+
+async function showSingle(word, repository) {
+  const dict = await repository.loadByWord(word);
+  const translation = dict[word];
+  return translation ? [[word, translation]] : [];
 }
