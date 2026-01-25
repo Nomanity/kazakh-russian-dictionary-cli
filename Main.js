5e78b6df94323479a2c13eb6ddc50ae066299ddd
Jack <leovski76@gmail.com>
2026-01-26 02:33:59 +0500

    feature(main): добавлена новая фича updateWord для изменения перевода

--- Main.js
+++ Main.js
@@ -2,6 +2,7 @@
 import { startCli } from "./UI/cli.js";
 import { addWord } from "./application/addWord.js";
 import { showWords } from "./application/showWords.js";
+import { updateWord } from "./application/updateWord.js";
 import { createRepository } from "./infrastructure/fileDictionaryRepository.js";
 
 const fileRepository = createRepository();
@@ -9,5 +10,6 @@ const fileRepository = createRepository();
 
 startCli({
     addWord: data => addWord(data, fileRepository),
-    showWords: query => showWords(query, fileRepository)
+    showWords: query => showWords(query, fileRepository),
+    updateWord: data => updateWord(data, fileRepository)
 });
